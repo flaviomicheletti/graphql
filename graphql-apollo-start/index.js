60fb63754f0a2e9d24fa4eb9b57f552bd8fce7c1
Flávio Micheletti <flaviomicheletti@hotmail.com>
2021-03-19 23:20:15 -0300

    first commit

---
+++ graphql-apollo-start/index.js
@@ -0,0 +1,57 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+//
+// Define your GraphQL schema
+//
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+//
+// Define your data set
+//
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+//
+// Define a resolver
+//
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+//
+// Create an instance of ApolloServer
+//
+
+// typeDefs:  your schemadefinition
+// resolvers: your set of resolvers
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
---
+++ graphql-apollo-start/readme.md
@@ -0,0 +1,10 @@
+# Apollo Graphql Getting Started
+
+This tutorial walks you through installing and configuring Apollo Server. 
+
+If you're just getting started with GraphQL or the Apollo platform, we recommend 
+first completing the [full-stack tutorial](https://www.apollographql.com/docs/tutorial/introduction/).
+
+https://www.apollographql.com/docs/apollo-server/getting-started/
+
+http://localhost:4000/
